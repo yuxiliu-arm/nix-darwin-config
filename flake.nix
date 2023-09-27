b7e27334a8b35e23e1677da2c3e1ff01603a7617
crvdgc <ubikium@gmail.com>
2023-09-27 12:18:40 +0100

    bump to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "rubeno";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-darwin = {
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
