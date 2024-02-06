0363857a5975fe407d7c17dd205b9ab94f51323b
crvdgc <ubikium@gmail.com>
2024-02-06 16:18:59 +0000

    add user to trusted-users

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -11,6 +11,7 @@
     keep-outputs = true  # Do not garbage-collect build time-only dependencies (e.g. clang)
     
     # Allow fetching build results from the Lean Cachix cache
+    trusted-users = yuxi
     trusted-substituters = https://lean4.cachix.org/
     trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= lean4.cachix.org-1:mawtxSxcaiWE24xCXXgh3qnvlTkyU7evRRnGeAhD4Wk=
     # }
