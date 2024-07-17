9702ce3c8a669cdc5de11c1085001b20a237065a
crvdgc <ubikium@gmail.com>
2024-07-17 10:59:39 +0100

    rust lsp config

--- neovim/lvim/config.lua
+++ neovim/lvim/config.lua
@@ -244,7 +244,65 @@ lvim.plugins = {
   },
   {
     "simrat39/rust-tools.nvim",
-    -- ft = { "rust", "rs" },
+    -- https://github.com/LunarVim/starter.lvim/blob/fdc38b5f4d95f81707f0606b49a444f9b93adf62/config.lua
+    config = function()
+      require("rust-tools").setup {
+        tools = {
+          executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+          reload_workspace_from_cargo_toml = true,
+          runnables = {
+            use_telescope = true,
+          },
+          inlay_hints = {
+            auto = true,
+            only_current_line = false,
+            show_parameter_hints = false,
+            parameter_hints_prefix = "<-",
+            other_hints_prefix = "=>",
+            max_len_align = false,
+            max_len_align_padding = 1,
+            right_align = false,
+            right_align_padding = 7,
+            highlight = "Comment",
+          },
+          hover_actions = {
+            border = "rounded",
+          },
+          on_initialized = function()
+            vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+              pattern = { "*.rs" },
+              callback = function()
+                local _, _ = pcall(vim.lsp.codelens.refresh)
+              end,
+            })
+          end,
+        },
+        -- dap = {
+        --   -- adapter= codelldb_adapter,
+        --   adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+        -- },
+        server = {
+          on_attach = function(client, bufnr)
+            require("lvim.lsp").common_on_attach(client, bufnr)
+            local rt = require "rust-tools"
+            vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+          end,
+
+          capabilities = require("lvim.lsp").common_capabilities(),
+          settings = {
+            ["rust-analyzer"] = {
+              lens = {
+                enable = true,
+              },
+              checkOnSave = {
+                enable = true,
+                command = "clippy",
+              },
+            },
+          },
+        },
+      }
+    end
   },
   {
     "phaazon/hop.nvim",
